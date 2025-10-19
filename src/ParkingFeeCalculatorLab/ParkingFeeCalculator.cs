44d7505516df0cadc918b58443f3d09726b2f03e
Anne <anne.lin.www@gmail.com>
2025-10-19 17:30:34 +0800

    feat: ep1 新增停車費計算機功能與單元測試

---
+++ src/ParkingFeeCalculatorLab/ParkingFeeCalculator.cs
@@ -0,0 +1,21 @@
+
+
+namespace ParkingFeeCalculatorLab
+{
+    public class ParkingFeeCalculator
+    {
+        // Topic: 如何製作一個「能上新聞的」停車費計算機
+        // 停車場
+        // 15 分鐘內免費
+        // 平日
+        //      每小時 60 元（以半小時計）
+        //      當日上限 150 元（隔日另計）
+        // 假日與國定假日
+        //      每小時 100 元（以半小時計）
+        //      當日上限 2400 元（隔日另計）
+        public long CalculateFee(DateTime start, DateTime end)
+        {
+            return 0;
+        }
+    }
+}
---
+++ tests/UnitTests/ParkingFeeCalculatorTest.cs
@@ -0,0 +1,25 @@
+﻿using ParkingFeeCalculatorLab;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class Tests
+    {  
+        private ParkingFeeCalculator _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new ParkingFeeCalculator();
+        }
+
+        [Test]
+        public void CalculateFee_15Minutes_Free()
+        {
+            DateTime start = new DateTime(2025, 1, 1, 0, 0, 0);
+            DateTime end = new DateTime(2025, 1, 1, 0, 14, 59);
+            long actual = _sut.CalculateFee(start, end);
+            Assert.That(actual, Is.EqualTo(0));
+        }
+    }
+}
