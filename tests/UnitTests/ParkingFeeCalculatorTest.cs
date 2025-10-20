a57b7d50b2363f92f59a7d2ac89f7ca98d97c6af
Anne <anne.lin.www@gmail.com>
2025-10-20 21:12:43 +0800

    refactor: ep3 extract temporary method

--- tests/UnitTests/ParkingFeeCalculatorTest.cs
+++ tests/UnitTests/ParkingFeeCalculatorTest.cs
@@ -17,46 +17,37 @@ namespace UnitTests
         [Test]
         public void CalculateFee_15Minutes_Free()
         {
-            DateTime start = new DateTime(2025, 1, 1, 0, 0, 0);
-            DateTime end = new DateTime(2025, 1, 1, 0, 14, 59);
-            long actual = _sut.CalculateFee(start, end);
-            actual.ShouldBe(0);
+            Run_Test(new DateTime(2025, 1, 1, 0, 0, 0), new DateTime(2025, 1, 1, 0, 14, 59), 0);
         }
 
         [Test]
         public void CalculateFee_Over_15Minutes_Not_Free()
         {
-            DateTime start = new DateTime(2025, 1, 1, 0, 0, 0);
-            DateTime end = new DateTime(2025, 1, 1, 0, 15, 0);
-            long actual = _sut.CalculateFee(start, end);
-            actual.ShouldBe(30);
+            Run_Test(new DateTime(2025, 1, 1, 0, 0, 0), new DateTime(2025, 1, 1, 0, 15, 0), 30);
         }
 
         [Test]
         public void CalculateFee_Over_30Minutes_Then_pay_60()
         {
-            DateTime start = new DateTime(2025, 1, 1, 0, 0, 0);
-            DateTime end = new DateTime(2025, 1, 1, 0, 30, 0);
-            long actual = _sut.CalculateFee(start, end);
-            actual.ShouldBe(60);
+            Run_Test(new DateTime(2025, 1, 1, 0, 0, 0), new DateTime(2025, 1, 1, 0, 30, 0), 60);
         }
         
         [Test]
         public void CalculateFee_Over_60Minutes_Then_pay_90()
         {
-            DateTime start = new DateTime(2025, 1, 1, 0, 0, 0);
-            DateTime end = new DateTime(2025, 1, 1, 1, 0, 0);
-            long actual = _sut.CalculateFee(start, end);
-            actual.ShouldBe(90);
+            Run_Test(new DateTime(2025, 1, 1, 0, 0, 0), new DateTime(2025, 1, 1, 1, 0, 0), 90);
         }
-        
+
         [Test]
         public void CalculateFee_Over_150Minutes_Then_pay_150()
         {
-            DateTime start = new DateTime(2025, 1, 1, 0, 0, 0);
-            DateTime end = new DateTime(2025, 1, 1, 2, 30, 0);
+            Run_Test(new DateTime(2025, 1, 1, 0, 0, 0), new DateTime(2025, 1, 1, 2, 30, 0), 150);
+        }
+
+        private void Run_Test(DateTime start, DateTime end, long expected)
+        {
             long actual = _sut.CalculateFee(start, end);
-            actual.ShouldBe(150);
+            actual.ShouldBe(expected);
         }
     }
 }
