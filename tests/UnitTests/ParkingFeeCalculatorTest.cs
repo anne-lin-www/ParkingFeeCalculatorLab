5d6b1c64b44e1dd391439925fcaaef026efa5ad1
Anne <anne.lin.www@gmail.com>
2025-10-20 21:20:36 +0800

    chore: 改為與影片範例一致，時間以字串變數傳入

--- tests/UnitTests/ParkingFeeCalculatorTest.cs
+++ tests/UnitTests/ParkingFeeCalculatorTest.cs
@@ -6,47 +6,45 @@ namespace UnitTests
 {
     public class Tests
     {
-        private ParkingFeeCalculator _sut;
-
         [SetUp]
         public void Setup()
         {
-            _sut = new ParkingFeeCalculator();
         }
 
         [Test]
         public void CalculateFee_15Minutes_Free()
         {
-            Run_Test(new DateTime(2025, 1, 1, 0, 0, 0), new DateTime(2025, 1, 1, 0, 14, 59), 0);
+            Run_Test("2025-01-01 00:00:00", "2025-01-01 00:14:59", 0);
         }
 
         [Test]
         public void CalculateFee_Over_15Minutes_Not_Free()
         {
-            Run_Test(new DateTime(2025, 1, 1, 0, 0, 0), new DateTime(2025, 1, 1, 0, 15, 0), 30);
+            Run_Test("2025-01-01 00:00:00", "2025-01-01 00:15:00", 30);
         }
 
         [Test]
         public void CalculateFee_Over_30Minutes_Then_pay_60()
         {
-            Run_Test(new DateTime(2025, 1, 1, 0, 0, 0), new DateTime(2025, 1, 1, 0, 30, 0), 60);
+            Run_Test("2025-01-01 00:00:00", "2025-01-01 00:30:00", 60);
         }
         
         [Test]
         public void CalculateFee_Over_60Minutes_Then_pay_90()
         {
-            Run_Test(new DateTime(2025, 1, 1, 0, 0, 0), new DateTime(2025, 1, 1, 1, 0, 0), 90);
+            Run_Test("2025-01-01 00:00:00", "2025-01-01 01:00:00", 90);
         }
 
         [Test]
         public void CalculateFee_Over_150Minutes_Then_pay_150()
         {
-            Run_Test(new DateTime(2025, 1, 1, 0, 0, 0), new DateTime(2025, 1, 1, 2, 30, 0), 150);
+            Run_Test("2025-01-01 00:00:00", "2025-01-01 02:30:00", 150);
         }
 
-        private void Run_Test(DateTime start, DateTime end, long expected)
+        private void Run_Test(string start, string end, long expected)
         {
-            long actual = _sut.CalculateFee(start, end);
+            ParkingFeeCalculator sut = new ParkingFeeCalculator();
+            long actual = sut.CalculateFee(DateTime.Parse(start),  DateTime.Parse(end));
             actual.ShouldBe(expected);
         }
     }
