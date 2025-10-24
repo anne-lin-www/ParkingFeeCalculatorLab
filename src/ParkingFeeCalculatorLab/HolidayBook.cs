2d7ada29a8299ae8c327718fa6238bbec359bce2
Anne <anne.lin.www@gmail.com>
2025-10-24 20:37:13 +0800

    refactor: ep7 move daily fee calculation to HolidayBook

--- src/ParkingFeeCalculatorLab/HolidayBook.cs
+++ src/ParkingFeeCalculatorLab/HolidayBook.cs
@@ -2,6 +2,7 @@
 
 public class HolidayBook
 {
+    private readonly TimeSpan THIRTY_MINUTES = TimeSpan.FromMinutes(30);
     private readonly List<DayOfWeek> _weekend = [DayOfWeek.Saturday, DayOfWeek.Sunday];
     private readonly HashSet<DateTime> _nationalHolidays;
 
@@ -17,5 +18,23 @@ public class HolidayBook
     public bool IsHoliday(DateTime today)
     {
         return _weekend.Contains(today.DayOfWeek) || _nationalHolidays.Contains(today.Date);
+    }
+
+    public long GetDailyFee(DailySession dailySession)
+    {
+        long todayFee = GetRegularFee(dailySession);
+        long dailyLimit = IsHoliday(dailySession.GetToday())
+            ? 2400L
+            : 150L; // TODO: 根據假日或國定假日，調整上限
+        return Math.Min(todayFee, dailyLimit);
+    }
+
+    private long GetRegularFee(DailySession dailySession)
+    {
+        // 以 30 分鐘為單位，無條件進位
+        long periods = (long)Math.Ceiling(dailySession.GetTodayDuration().TotalMinutes / THIRTY_MINUTES.TotalMinutes);
+        return periods * (IsHoliday(dailySession.GetToday())
+            ? 50
+            : 30);
     }
 }
--- src/ParkingFeeCalculatorLab/ParkingFeeCalculator.cs
+++ src/ParkingFeeCalculatorLab/ParkingFeeCalculator.cs
@@ -1,82 +1,67 @@
-namespace ParkingFeeCalculatorLab
+namespace ParkingFeeCalculatorLab;
+
+public class ParkingFeeCalculator
 {
-    public class ParkingFeeCalculator
+    private readonly HolidayBook holidayBook;
+    private readonly TimeSpan FIFTEEN_MINUTES = TimeSpan.FromMinutes(15);
+    // Topic: 如何製作一個「能上新聞的」停車費計算機
+    // 停車場
+    // 15 分鐘內免費
+    // 平日
+    //      每小時 60 元（以半小時計）
+    //      當日上限 150 元（隔日另計）
+    // 假日與國定假日
+    //      每小時 100 元（以半小時計）
+    //      當日上限 2400 元（隔日另計）
+    //
+    // [0, 15] mins → 0 => Special Case
+    // (15, 30] mins → 30
+    // (30, 60] mins → 60
+    // (60, 90] mins → 90
+    // (90, 120] mins → 120
+    // (120, 150] 分鐘 → 150
+
+    public ParkingFeeCalculator()
     {
-        private readonly HolidayBook holidayBook = new HolidayBook();
-        private readonly TimeSpan THIRTY_MINUTES = TimeSpan.FromMinutes(30);
-        private readonly TimeSpan FIFTEEN_MINUTES = TimeSpan.FromMinutes(15);
-        // Topic: 如何製作一個「能上新聞的」停車費計算機
-        // 停車場
-        // 15 分鐘內免費
-        // 平日
-        //      每小時 60 元（以半小時計）
-        //      當日上限 150 元（隔日另計）
-        // 假日與國定假日
-        //      每小時 100 元（以半小時計）
-        //      當日上限 2400 元（隔日另計）
-        //
-        // [0, 15] mins → 0 => Special Case
-        // (15, 30] mins → 30
-        // (30, 60] mins → 60
-        // (60, 90] mins → 90
-        // (90, 120] mins → 120
-        // (120, 150] 分鐘 → 150
-
-        public long CalculateFee(ParkingSession parkingSession)
-        {
-            // 跨天
-            //      一個 duration 切多段，一天一段
-            // 例假日與國定假日
-            //      今天是哪一天
-            //      一個 duration 切多段，一天一段
-
-            var duration = parkingSession.GetTotalDuration();
-
-            if (IsShort(duration))
-            {
-                return 0L;
-            }
-            
-            // Data Clumps + Primitive Obsession
-            // Lack of domain knowledge
-            // each iteration in the loop:
-            //     calculate daily duration             => parking behavior
-            //     calculate fee with daily duration    => charging behavior
-
-            // 透過加上「假日計費」的邏輯，來把「算帳的領域物件」「逼」出來。
-            
-            var dailySessions = parkingSession.GetDailySessions();
-            
-            long totalFee = 0L;
-            foreach (var dailySession in dailySessions)
-            {
-                long todayFee = GetRegularFee(dailySession);
-                long dailyLimit = holidayBook.IsHoliday(dailySession.GetToday())
-                    ? 2400L
-                    : 150L; // TODO: 根據假日或國定假日，調整上限
-                totalFee += Math.Min(todayFee, dailyLimit);
-            }
-            
-            return totalFee;
-        }
-
-        private long GetRegularFee(DailySession dailySession)
-        {
-            // 以 30 分鐘為單位，無條件進位
-            long periods = (long)Math.Ceiling(dailySession.GetTodayDuration().TotalMinutes / THIRTY_MINUTES.TotalMinutes);
-            return periods * (holidayBook.IsHoliday(dailySession.GetToday())
-                ? 50
-                : 30);
-        }
-
-        private bool IsShort(TimeSpan duration)
-        {
-            return duration.TotalMinutes <= FIFTEEN_MINUTES.TotalMinutes;
-        }
-        
-        private bool IsHoliday(DateTime today)
-        {
-            return holidayBook.IsHoliday(today);
-        }
+        holidayBook = new HolidayBook();
     }
+
+    public long CalculateFee(ParkingSession parkingSession)
+    {
+        var duration = parkingSession.GetTotalDuration();
+
+        if (IsShort(duration))
+        {
+            return 0L;
+        }
+            
+        // Data Clumps + Primitive Obsession
+        // Lack of domain knowledge
+        // each iteration in the loop:
+        //     calculate daily duration             => parking behavior
+        //     calculate fee with daily duration    => charging behavior
+
+        // 透過加上「假日計費」的邏輯，來把「算帳的領域物件」「逼」出來。
+            
+        var dailySessions = parkingSession.GetDailySessions();
+            
+        long totalFee = 0L;
+        foreach (var dailySession in dailySessions)
+        {
+            var daily = holidayBook.GetDailyFee(dailySession);
+            totalFee += daily;
+        }
+            
+        return totalFee;
+    }
+
+    private bool IsShort(TimeSpan duration)
+    {
+        return duration.TotalMinutes <= FIFTEEN_MINUTES.TotalMinutes;
+    }
+        
+    // private bool IsHoliday(DateTime today)
+    // {
+    //     return holidayBook.IsHoliday(today);
+    // }
 }
